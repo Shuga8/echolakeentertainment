fd5680096d5497e3df60bfe5416d83fcaef4230f
Shuga8 <lotocharles8@gmail.com>
2024-06-07 10:20:13 +0100

    Configurations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,22 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\RedirectIfAdmin;
+use App\Http\Middleware\RedirectIfNotAdmin;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => RedirectIfNotAdmin::class,
+            'admin.guest' => RedirectIfAdmin::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::namespace('Admin')->prefix('admin')->name('admin')->group(function () {
+    Route::namespace('Auth')->prefix('auth')->name('auth')->group(function () {
+        Route::controller('AuthController')->group(function () {
+            Route::get('', 'index')->name('login');
+        });
+    });
+});
