3c2f2fd531a499376c1f4fd967260d7f55ad32ef
Shuga8 <lotocharles8@gmail.com>
2024-06-10 19:23:17 +0100

    admin logins,model && migration

---
+++ database/migrations/2024_06_10_181148_create_admin_logins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_logins', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('admin_id');
+            $table->string('admin_ip')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country_code')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('browser')->nullable();
+            $table->string('os')->nullable();
+            $table->timestamps();
+            $table->foreign('admin_id')
+                ->references('id')
+                ->on('admin')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_logins');
+    }
+};
